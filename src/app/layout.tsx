b2305aed882152f84559a22ed4191c74439659d8
Yosuf <youssefsadiq66@gmail.com>
2025-10-31 00:24:15 +0200

    Unify all fonts to use system font stack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,6 @@
 import type { Metadata, Viewport } from 'next';
-import { EB_Garamond, Amiri } from 'next/font/google';
 import { LanguageProvider } from '@/contexts/LanguageContext';
 import './globals.css';
-
-// Preload fonts with display swap for better performance
-const garamond = EB_Garamond({ 
-  subsets: ['latin'], 
-  variable: '--font-garamond',
-  weight: ['400', '500', '600', '700'],
-  display: 'swap',
-  preload: true,
-});
-
-const amiri = Amiri({ 
-  subsets: ['arabic'], 
-  variable: '--font-arabic',
-  weight: ['400', '700'],
-  display: 'swap',
-  preload: true,
-});
 
 export const metadata: Metadata = {
   title: 'Marwan Ahmed Awny Elagha | Arab Scout Committee Candidate',
@@ -64,31 +46,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://www.google-analytics.com" />
         <link rel="preconnect" href="https://www.googletagmanager.com" />
         <link rel="preconnect" href="https://formspree.io" />
-        
-        {/* Preload critical CSS */}
-        <style dangerouslySetInnerHTML={{
-          __html: `
-            @font-face {
-              font-family: 'EB Garamond';
-              font-style: normal;
-              font-weight: 400 700;
-              font-display: swap;
-              src: url('/fonts/eb-garamond.woff2') format('woff2');
-              unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-            }
-            
-            @font-face {
-              font-family: 'Amiri';
-              font-style: normal;
-              font-weight: 400 700;
-              font-display: swap;
-              src: url('/fonts/amiri.woff2') format('woff2');
-              unicode-range: U+0600-06FF, U+200C-200E, U+2010-2011, U+204F, U+2E41, U+FB50-FDFF, U+FE80-FEFC;
-            }
-          `
-        }} />
       </head>
-      <body className={`${garamond.variable} ${amiri.variable} font-sans antialiased`}>
+      <body className="antialiased">
         <LanguageProvider>{children}</LanguageProvider>
       </body>
     </html>
